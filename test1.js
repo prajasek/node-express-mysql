6781e10b9e39ce3df76b4dd3c9f9cdcdd4ad7f4f
Prasanth <sekaranp92@gmail.com>
2022-07-10 23:19:37 -0400

    removed commented out code

--- test1.js
+++ test1.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 let instance = axios.create({
     timeout: 222456,
     headers : {
-        // 'Authorization' : 'testAuthToken',
+        // 'Authorization' : 'testAuthToken', -> takes precedence over defaults
         common: {
             'testHeaderCommon' : 'testCommonHeader'
         }
@@ -14,22 +14,14 @@ let instance = axios.create({
 })
 
 let token = jwt.sign({token: 'token_data'}, "secret", {expiresIn: 10});
-console.log("Start")
-
 instance.defaults.headers.common['cookie'] = 'testCookie';
 instance.defaults.headers.common.Authorization = `Bearer ${token}`;
-instance.get('https://www.google.com', {
-    params: {"Q":'123'}
+instance.get('https://www.google.com', {params: {"Q":'123'}
 }).then(resp => {
-    // for (const i in resp) {
-    //     console.log(i);
-    // }
     console.log("\n\n", resp.config);
     // console.log(resp.statusText);
     insert = `${resp.status}, '${resp.statusText}', ${resp.status}`;
     //f(insert);
-
-
     jwt.verify(token, 'secret', (err, decode) => {
         if (err) return console.log("Decode Error \n ----", err);
         console.log("Decoded -->", decode);
@@ -37,31 +29,16 @@ instance.get('https://www.google.com', {
         console.log("c = ", c)
     });
     setTimeout(() => {
-        
         jwt.verify(token, 'secret', (err, decode) => {
             if (err) return console.log("Decode2 Error \n ----", err);
             console.log("Decoded2 -->", decode);
             c = jwt.decode(token);
             console.log("c = ", c)
         })}, 1000)
-
-    console.log("End")
-
-
-
-    // console.log("insert=", insert)
-    // resp.headers['set-cookie'].forEach(x => {
-    //     console.log(x.split(';').filter(x =>
-    //         x.startsWith(' domain')));
-    //     console.log(x, "\n\n");
-    // })
+    
 }).catch(resp => {
-    //console.log(resp = resp.response);
-    //insert = `${resp.status}, '${resp.statusText}', ${resp.status}`;
-    //f(insert)
-    console.log("error")
-})
-
+        console.log("error")
+    })
 
 function f(data) {
     console.log(`(${data})`)
